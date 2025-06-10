d54b3e7d935a4da829767bc3fe6eebf9c86d87a9
jboyxs <3408970170@qq.com>
2025-06-10 18:19:28 +0800

    疑似找不到32位深度的ram/rom/blockmem/distributemem

--- ArmPipleCPU.srcs/sources_1/new/ifstage.v
+++ ArmPipleCPU.srcs/sources_1/new/ifstage.v
@@ -21,6 +21,36 @@
 
 
 module ifstage(
-
+    input clk,
+    inout [31:0] pcforward,
+    input [31:0] pcdelay,
+    input  pcsrcw,
+    input branchtakene,
+    input stallf
+    output [31:0] instrf;
     );
+    reg [31:0] pc1,pc2,pcf;
+    wire [31:0] pcplus4;
+    
+    // Fixed memory module instantiation with proper ports
+    // dist_men_im imem(.port1(), .port2(), ...); // Uncomment and add proper ports
+   
+    always @(*)
+    begin
+    if (pcsrcw == 1'b1) 
+        pc1 = pcdelay;
+    else pc1 = pcplus4;
+    if (branchtakene == 1'b1) 
+        pc2 = pcforward;
+    else pc2 = pc1;
+    end
+    
+    assign pcplus4 = pcf+4;
+    
+    always @(posedge clk)
+    begin
+        if (!stallf)
+            pcf = pc2;
+    end
+
 endmodule
