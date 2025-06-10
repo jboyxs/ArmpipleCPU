d98be4901f8bde76b4ce8da0597883ace3a8df74
jboyxs <3408970170@qq.com>
2025-06-10 17:48:55 +0800

    stage tryve

---
+++ ArmPipleCPU.gen/sources_1/ip/dist_mem_im/dist_mem_im_sim_netlist.vhdl
@@ -0,0 +1,72 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+-- Date        : Tue Jun 10 17:47:57 2025
+-- Host        : Firstputve running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode funcsim
+--               c:/Users/jjboy/ArmPipleCPU/ArmPipleCPU.gen/sources_1/ip/dist_mem_im/dist_mem_im_sim_netlist.vhdl
+-- Design      : dist_mem_im
+-- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
+--               synthesized. This netlist cannot be used for SDF annotated simulation.
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity dist_mem_im is
+  port (
+    a : in STD_LOGIC_VECTOR ( 5 downto 0 );
+    spo : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+  attribute NotValidForBitStream : boolean;
+  attribute NotValidForBitStream of dist_mem_im : entity is true;
+  attribute CHECK_LICENSE_TYPE : string;
+  attribute CHECK_LICENSE_TYPE of dist_mem_im : entity is "dist_mem_im,dist_mem_gen_v8_0_15,{}";
+  attribute downgradeipidentifiedwarnings : string;
+  attribute downgradeipidentifiedwarnings of dist_mem_im : entity is "yes";
+  attribute x_core_info : string;
+  attribute x_core_info of dist_mem_im : entity is "dist_mem_gen_v8_0_15,Vivado 2024.2";
+end dist_mem_im;
+
+architecture STRUCTURE of dist_mem_im is
+  signal \<const0>\ : STD_LOGIC;
+begin
+  spo(31) <= \<const0>\;
+  spo(30) <= \<const0>\;
+  spo(29) <= \<const0>\;
+  spo(28) <= \<const0>\;
+  spo(27) <= \<const0>\;
+  spo(26) <= \<const0>\;
+  spo(25) <= \<const0>\;
+  spo(24) <= \<const0>\;
+  spo(23) <= \<const0>\;
+  spo(22) <= \<const0>\;
+  spo(21) <= \<const0>\;
+  spo(20) <= \<const0>\;
+  spo(19) <= \<const0>\;
+  spo(18) <= \<const0>\;
+  spo(17) <= \<const0>\;
+  spo(16) <= \<const0>\;
+  spo(15) <= \<const0>\;
+  spo(14) <= \<const0>\;
+  spo(13) <= \<const0>\;
+  spo(12) <= \<const0>\;
+  spo(11) <= \<const0>\;
+  spo(10) <= \<const0>\;
+  spo(9) <= \<const0>\;
+  spo(8) <= \<const0>\;
+  spo(7) <= \<const0>\;
+  spo(6) <= \<const0>\;
+  spo(5) <= \<const0>\;
+  spo(4) <= \<const0>\;
+  spo(3) <= \<const0>\;
+  spo(2) <= \<const0>\;
+  spo(1) <= \<const0>\;
+  spo(0) <= \<const0>\;
+GND: unisim.vcomponents.GND
+     port map (
+      G => \<const0>\
+    );
+end STRUCTURE;
